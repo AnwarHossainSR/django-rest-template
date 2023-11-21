c370d5f8aa7a1e79a6da58eba1c99e4fdd94b706
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-11-21 15:54:20 +0600

    create apis

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length=200, blank=True, default='')
+    completed = models.BooleanField(default=False)
